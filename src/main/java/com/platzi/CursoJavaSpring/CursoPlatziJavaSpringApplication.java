edcfc7e998fe7147e1ac386276ca6f60ade816fe
Daniel <danielcossio195@gmail.com>
2023-07-24 18:09:58 -0500

    Plantilla de proyecto

---
+++ src/main/java/com/platzi/CursoJavaSpring/CursoPlatziJavaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.platzi.CursoJavaSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoPlatziJavaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoPlatziJavaSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/platzi/CursoJavaSpring/CursoPlatziJavaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.platzi.CursoJavaSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoPlatziJavaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
